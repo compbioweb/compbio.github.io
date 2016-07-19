38e754d6e4f1652cc3d5da172b84b531fb4b04bf
benmbrew <benmbrew@gmail.com>
2016-07-19 14:10:12 -0400

    added assoc faculty

--- code/resize_code.py
+++ code/resize_code.py
@@ -3,13 +3,13 @@
 import PIL
 from PIL import Image
 import os
-os.chdir('/home/benbrew/Documents/compbioweb.github.io/images')
+os.chdir('/home/benbrew/Desktop/pic')
 
 
-for number in range(12)[1:]:
+for number in range(9)[1:]:
     img = Image.open(str(number) + '.jpg')
-    img = img.resize((180, 205), PIL.Image.ANTIALIAS)
-    img.save(str(number) + '_new.jpg')
+    img = img.resize((150, 190), PIL.Image.ANTIALIAS)
+    img.save(str(number) + '_assoc.jpg')
 
 
 
